96550f280c954a3545c4859a93a347801ca30e86
aulunax <kamilony@gmail.com>
2024-12-07 19:17:14 +0100

    formatting fix

--- include/Btree/BtreePage.h
+++ include/Btree/BtreePage.h
@@ -9,44 +9,40 @@
 
 class BtreePage
 {
-public:
     int parentOffset;
     int thisPageOffset;
     std::vector<BtreeNode> nodes;
 
-    BtreePage() {};
-
 public:
-    // BtreePage();
+    BtreePage() : parentOffset(NULL_DATA), thisPageOffset(NULL_DATA) {};
     BtreePage(int parentOffset) : parentOffset(parentOffset) {}
     BtreePage(int parentOffset, std::vector<BtreeNode> nodes);
+
     void addNode(BtreeNode node);
     void insertNode(BtreeNode node);
     void clearNodes();
 
     int getRecordsOnPageCount() const;
     int getParentOffset() const;
-    void setParentOffset(int parentOffset);
     int getThisPageOffset() const { return thisPageOffset; }
+    std::vector<BtreeNode> &getNodes();
+
+    void setParentOffset(int parentOffset);
     void setThisPageOffset(int thisPageOffset) { this->thisPageOffset = thisPageOffset; }
-    int serialize(std::unique_ptr<char[]> &serializedPage) const;
 
     std::pair<int, bool> bisectionSearchForKey(int key);
 
-    std::vector<BtreeNode> &getNodes();
-
-    bool compensation();
-    bool split();
+    bool isRoot();
 
     BtreeNode getNode(int index) const;
     int getKey(int index) const;
-    void setKey(int index, int value);
     int getPtr(int index) const;
     int getRecordOffset(int index) const;
+
+    void setKey(int index, int value);
     void setRecordOffset(int index, int value);
 
-    bool isRoot();
-
+    int serialize(std::unique_ptr<char[]> &serializedPage) const;
     static BtreePage deserialize(const std::unique_ptr<char[]> &serializedPage);
 };
 
